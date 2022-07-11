18d0bf06f3a9743c40b2b5ee9fa70710da96ae41
Stephen Liedig <sliedig@amazon.com>
2022-07-11 13:27:34 +0800

    chore: removed command to delete workshop directory which no longer exists.

--- code/lab-0/configureCloud9.sh
+++ code/lab-0/configureCloud9.sh
@@ -5,7 +5,6 @@ sudo yum update -y
 pip install cfn-lint --use-feature=2020-resolver
 
 #cleanup
-rm -fr ~/environment/wild-rydes-async-messaging/workshop/
 rm -fr ~/environment/wild-rydes-async-messaging/.git
 mv ~/environment/wild-rydes-async-messaging/code/lab-* ~/environment/wild-rydes-async-messaging/
 rm -fr ~/environment/wild-rydes-async-messaging/code/
